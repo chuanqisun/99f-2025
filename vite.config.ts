3e6856fd78a631db24e567c83aa2642db1f5bcec
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-10-17 01:26:13 -0400

    add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/99f-2025/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        certificate: resolve(__dirname, "certificate.html"),
+        details: resolve(__dirname, "details.html"),
+        host: resolve(__dirname, "host.html"),
+        humanVow: resolve(__dirname, "human-vow.html"),
+      },
+    },
+  },
 });
